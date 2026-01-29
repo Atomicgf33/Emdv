183e0a0dd4a72fc2560c2136bc2bb7841160a32b
Guilherme <guilherme.avilf@gmail.com>
2026-01-29 00:44:15 -0300

    pra ser lindo so falta esse

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.io.IOException;
-//vamos dormir cadu 2
+//vamos dormir cadu lambe cu
 public class Main {
     public static void main(String[] args) throws IOException {
 
